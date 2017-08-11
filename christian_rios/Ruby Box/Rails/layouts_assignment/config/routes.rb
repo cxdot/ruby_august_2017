64ce2ba188a58aeabe1a9379a10e3c0e4cb3f157
cxdot <crios_23@yahoo.com>
2017-08-11 16:03:08 -0500

    minor change

--- christian_rios/Ruby Box/Rails/layouts_assignment/config/routes.rb
+++ christian_rios/Ruby Box/Rails/layouts_assignment/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # resources :users do
-  #   resources :posts
-# end
-    resources :users
-    resources :posts
-
+  resources :users do
+    resources :posts, only:[:create]
+end
+  resources :posts, only:[:index]
+    # resources :users
+    # resources :posts
+    #
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
