9abb0a010526ca5465bc19e00419324c1e2fd56f
cxdot <crios_23@yahoo.com>
2017-08-13 14:26:19 -0500

    oop

---
+++ christian_rios/Ruby Box/OOP/wizard_ninja_samurai/human.rb
@@ -0,0 +1,25 @@
+ass Human
+  attr_accessor :strengh, :intelligence, :stealth, :health
+
+  def initialize
+  @strength =
+  @intelligence = 3
+  @stealth = 3
+  @health = 100
+  end
+
+  def attack(object)
+    if object.class.ancestors.include?(Human)
+      object.health -= 10
+      true
+    else
+      false
+    end
+  end
+end
+
+h = Human.new
+h2 = Human.new
+puts h.attack(h2)
+puts h.attack("Not a human")
+puts h2.health
