d7c7a07f470c376d7a355a04478ee23d8ccbf8e0
cxdot <crios_23@yahoo.com>
2017-08-13 15:16:22 -0500

    friends

--- christian_rios/Ruby Box/Rails/friends/db/migrate/20170813193709_create_friendships.rb
+++ christian_rios/Ruby Box/Rails/friends/db/migrate/20170813193709_create_friendships.rb
@@ -1,7 +1,8 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
-      t.references :user, index: true, foreign_key: true
+      t.integer :user_id
+      t.integer :friend_id
 
       t.timestamps null: false
     end
--- christian_rios/Ruby Box/Rails/friends/db/schema.rb
+++ christian_rios/Ruby Box/Rails/friends/db/schema.rb
@@ -11,15 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170813193118) do
+ActiveRecord::Schema.define(version: 20170813193709) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
+    t.integer  "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  add_index "friendships", ["user_id"], name: "index_friendships_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
