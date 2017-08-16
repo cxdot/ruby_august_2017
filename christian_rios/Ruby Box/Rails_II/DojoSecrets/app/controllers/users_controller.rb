6df527b8d396adc17b6e644b7207f89d750f0fa7
cxdot <crios_23@yahoo.com>
2017-08-16 13:08:46 -0500

    user settings

--- christian_rios/Ruby Box/Rails_II/DojoSecrets/app/controllers/users_controller.rb
+++ christian_rios/Ruby Box/Rails_II/DojoSecrets/app/controllers/users_controller.rb
@@ -16,8 +16,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  @user = User.find(params[:id])
+    if @user.update user_params
+      redirect_to "/users/#{@user.id}"
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to "/users/#{@user.id}/edit"
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      reset_session
+      redirect_to "/users/new"
+    else
+      redirect_to "/users/#{@user.id}/edit"
+    end
+  end
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
